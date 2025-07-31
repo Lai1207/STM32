af9283ce24f2e1381c07aba1de43b1146f599f42
Lai1207 <k120733381@gmail.com>
2025-07-31 14:47:41 +0800

    create 3-5

---
+++ 3-5_STM32_button_controller_buzzer_driver/User/main.c
@@ -0,0 +1,24 @@
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+#include "Buzzer.h"
+#include "LightSensor.h"
+
+
+int main(void)
+{
+	Buzzer_Init();
+	LightSensor_Init();
+	while(1)
+	{	
+	if(LightSensor_Get()== 0)
+	{
+		Buzzer_OFF();
+	}
+	else
+	{
+		Buzzer_ON();
+	}
+		
+	}
+}
+ 
