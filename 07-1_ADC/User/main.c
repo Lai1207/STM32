b3c223e3ce44d4e17c678f52f7eb732e7f3e37bf
marklai <k120733381@gmail.com>
2026-04-08 13:53:35 +0800

    update ADC main function

--- 07-1_ADC/User/main.c
+++ 07-1_ADC/User/main.c
@@ -3,18 +3,26 @@
 #include "OLED.h"
 #include "AD.h"
 
-uint16_t ADValue;
+uint16_t ADValue, Volatge;
 int main(void)
 {
 	OLED_Init();
 	AD_Init();
-	
+	OLED_ShowString(1, 1, "ADValue");
+	OLED_ShowString(2, 1, "Volatge:0.00V");
 	
 	
 	
 	while(1)
 	{	
 		ADValue = AD_GetValue();
+		Volatge = (float)ADValue / 4095 *3.3;
+		OLED_ShowNum(1, 9, ADValue, 4);
+		OLED_ShowNum(2, 9, Volatge, 1);
+		OLED_ShowNum(2,11, (uint16_t)(Volatge * 100) % 100, 2);
+	
+	
+	
 	}
 }
  
